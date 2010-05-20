5b07a259759373036deaf4bc02dba20f0097cf66
Rune Botten <rbotten@gmail.com>
2010-05-20 17:57:18 +0200

    Cleanup tests for Station

--- spec/trafikanten/station_spec.rb
+++ spec/trafikanten/station_spec.rb
@@ -4,58 +4,60 @@ require File.dirname(__FILE__) + '/../../lib/trafikanten/station'
 
 describe Trafikanten::Station do
   
-  it 'can search for funky characters' do
-    stations = Trafikanten::Station.find_all_by_name('Ullevåll')
-    stations.size.should == 10
+  context 'search' do
+    it 'can search for funky characters' do
+      stations = Trafikanten::Station.find_all_by_name('Ullevåll')
+      stations.size.should == 10
 
-    # Test first
-    station = stations[0]
-    station.name.should == "Ullev&#229;l hageby # (Oslo)"
-    station.id.should == '1000013064'
-    station.type.should == '2'
-  end
-  
-  it 'can search for multiple stations by name' do
-    stations = Trafikanten::Station.find_all_by_name('Hels')
-    stations.size.should == 20
+      # Test first
+      station = stations[0]
+      station.name.should == "Ullev&#229;l hageby # (Oslo)"
+      station.id.should == '1000013064'
+      station.type.should == '2'
+    end
     
-    # Test first
-    station = stations[0]
-    station.name.should == "Helseheimen (Ullensaker)"
-    station.id.should == '02350113'
-    station.type.should == '1'
+    it 'can search for multiple stations by name' do
+      stations = Trafikanten::Station.find_all_by_name('Hels')
+      stations.size.should == 20
+
+      # Test first
+      station = stations[0]
+      station.name.should == "Helseheimen (Ullensaker)"
+      station.id.should == '02350113'
+      station.type.should == '1'
+
+      # Test middle
+      station = stations[9]
+      station.name.should == "Helsfyr # (Oslo)"
+      station.id.should == '1000021179'
+      station.type.should == '2'
+    end
     
-    # Test middle
-    station = stations[9]
-    station.name.should == "Helsfyr # (Oslo)"
-    station.id.should == '1000021179'
-    station.type.should == '2'
-  end
-  
-  it 'returns an empty array when searching for multiple stations and did not find any' do
-    stations = Trafikanten::Station.find_all_by_name('4892384923')
-    stations.should == []
-  end
-  
-  it 'can search for a specific station by name' do
-    station = Trafikanten::Station.find_by_name('Helsfyr [T-bane]')
-    station.class.should == Trafikanten::Station
-    station.name.should == 'Helsfyr [T-bane] (Oslo)'
-    station.id.should == "03011440"
-    station.type.should == "1"
+    it 'returns an empty array when searching for multiple stations and did not find any' do
+      stations = Trafikanten::Station.find_all_by_name('4892384923')
+      stations.should == []
+    end
     
-    station = Trafikanten::Station.find_by_name('Helsfyr [T-bane] (Oslo)')
-    station.class.should == Trafikanten::Station
-    station.name.should == 'Helsfyr [T-bane] (Oslo)'
-    station.id.should == "03011440"
-    station.type.should == "1"
-  end
-  
-  it 'returns nil when searching for a specific station and did not find any' do
-    station = Trafikanten::Station.find_by_name('8742374892374923')
-    station.should be_nil
-  end
-  
+    it 'can search for a specific station by name' do
+      station = Trafikanten::Station.find_by_name('Helsfyr [T-bane]')
+      station.class.should == Trafikanten::Station
+      station.name.should == 'Helsfyr [T-bane] (Oslo)'
+      station.id.should == "03011440"
+      station.type.should == "1"
+
+      station = Trafikanten::Station.find_by_name('Helsfyr [T-bane] (Oslo)')
+      station.class.should == Trafikanten::Station
+      station.name.should == 'Helsfyr [T-bane] (Oslo)'
+      station.id.should == "03011440"
+      station.type.should == "1"
+    end
+    
+    it 'returns nil when searching for a specific station and did not find any' do
+      station = Trafikanten::Station.find_by_name('8742374892374923')
+      station.should be_nil
+    end
+    
+  end  
   
   context 'type' do
     it 'is guessed based on ID length when not known' do
