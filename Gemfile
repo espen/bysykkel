ede937e17a950f7d021da21191159d2d8b4c9a05
Rune Botten <rbotten@gmail.com>
2010-05-22 14:53:49 +0200

    Fix dependencies, add Gemfile and GeoUtm

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Edit this Gemfile to bundle your application's dependencies.
+source 'http://gemcutter.org'
+source 'http://gems.github.com'
+
+gem 'nokogiri'
+gem 'tallakt-geoutm', '0.0.3',:as => 'geoutm'
--- lib/trafikanten.rb
+++ lib/trafikanten.rb
@@ -1,5 +1,10 @@
+require 'open-uri'
+require 'iconv'
+require 'cgi'
+
 require 'rubygems'
 require 'nokogiri'
+require 'geoutm'
 
 require File.dirname(__FILE__) + '/trafikanten/utils'
 require File.dirname(__FILE__) + '/trafikanten/station'
