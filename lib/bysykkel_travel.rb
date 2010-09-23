429d1c05f93c828cc06586bd46a6934a9c681d82
Espen Antonsen <espen@inspired.no>
2010-09-23 18:18:35 +0200

    bysykkel now

---
+++ lib/bysykkel_travel.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'iconv'
+require 'cgi'
+
+require 'rubygems'
+require 'nokogiri'
+require 'geoutm'
+
+require File.dirname(__FILE__) + '/bysykkel_travel/utils'
+require File.dirname(__FILE__) + '/bysykkel_travel/rack'
+
+module BysykkelTravel;end
