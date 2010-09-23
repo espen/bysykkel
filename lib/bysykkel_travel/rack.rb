1e7f0ab39a35298e4da79ac450e9b68092235b22
Espen Antonsen <espen@inspired.no>
2010-09-23 21:47:10 +0200

    include all attributes

--- lib/bysykkel_travel/rack.rb
+++ lib/bysykkel_travel/rack.rb
@@ -2,7 +2,7 @@ module BysykkelTravel
   class Rack
     BASE_URL = 'http://smartbikeportal.clearchannel.no/public/mobapp/maq.asmx/getRack?id=%s'
     
-    attr_accessor :description, :id, :type, :lat, :lng, :empty_locks, :ready_bikes, :online
+    attr_accessor :description, :id, :lat, :lng, :empty_locks, :ready_bikes, :online
     
     def initialize(attrs = {})
       attrs.each do |k,v|
@@ -12,12 +12,15 @@ module BysykkelTravel
     
     # Query the Travel XML API @ clearchannel.no for Racks.
     def self.find_by_id(id)
-      raw = open(BASE_URL % CGI.escape(id))
+      raw = open(BASE_URL % id )
       doc = Nokogiri::XML.parse raw
       rack = Nokogiri::XML(doc.children[0].children[0].text).children[0]
       return {
           :id => id, 
           :description => rack.xpath('description').text,
+          :empty_locks => rack.xpath('empty_locks').text,
+          :ready_bikes => rack.xpath('ready_bikes').text,
+          :online => rack.xpath('online').text,
           :lat => rack.xpath('latitude').text,
           :lng => rack.xpath('longitute').text
         }
