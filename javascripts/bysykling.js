72d8ee8a6f0c023dddada969d41bba7bda1b9824
Espen Antonsen <espen@inspired.no>
2011-04-13 13:41:51 +0800

    initial commit

---
+++ javascripts/bysykling.js
@@ -0,0 +1,152 @@
+Array.prototype.contains = function (element) {
+    for (var i = 0; i < this.length; i++) 
+    {
+        if (this[i] == element) {
+            return true;
+        }
+    }
+    return false;
+};
+  
+function listAll() {
+    $( '#all div[data-role="content"] ul' ).remove();
+	$.getJSON('http://bysykling.heroku.com/api/bysykkel/v1/racks/?callback=?', function(data) {
+        listed = localStorage.getItem("racks") ? localStorage.getItem("racks").split(';') : [];
+        list = $( "<ul>", { 
+    			"class": "ui-listbox-list",
+    		}).appendTo( '#all div[data-role="content"]' );
+
+		data.racks.forEach( function(rack,i) {
+		    if ( !listed.contains(rack.id) ) {
+    		    writeRack('all', rack,false);
+		    }
+		});
+        list.listview();
+	});
+}
+
+function listNearby() {
+    $( '#nearby div[data-role="content"] ul' ).remove();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( function(position) {
+            current_location = new LatLon( position.coords.latitude, position.coords.longitude );
+            list = $( "<ul>", { 
+        			"class": "ui-listbox-list",
+        		}).appendTo( '#nearby div[data-role="content"]' );
+            
+            $.getJSON('http://bysykling.heroku.com/api/bysykkel/v1/racks/live/?callback=?', function(data) {
+                nearRacks = [];
+        		data.racks.forEach( function(rack,i) {
+        		    rack_location = new LatLon( parseFloat( rack.geo.coordinates[1] ), parseFloat( rack.geo.coordinates[0] ) );
+    		        rack.distance = parseFloat( current_location.distanceTo(rack_location ) );
+        		    if ( rack.distance < 0.5 ) {
+            		    nearRacks.push( [ parseFloat( current_location.distanceTo(rack_location ) ), rack ] );
+        		    }
+        		});
+        		nearRacks.sort( function(a,b) {
+        		    return a[0] - b[0];
+        		} );
+        		nearRacks.forEach( function(item,i) {
+            		writeRack('nearby', item[1] );
+    		    });
+    		    list.listview();
+        	});
+        	
+        } );
+    }
+}
+
+function listRacks() {
+    var racks = localStorage.getItem("racks") ? localStorage.getItem("racks").split(';') : [];
+	var count = racks.length;
+    $( '#favorites div[data-role="content"] ul' ).remove();
+    list = $( "<ul>", { 
+			"class": "ui-listbox-list",
+		}).appendTo( '#favorites div[data-role="content"]' );
+	racks.forEach( function(rack,i) {
+		$.getJSON('http://bysykling.heroku.com/api/bysykkel/v1/racks/' + rack + '?callback=?', function(data) {
+		    writeRack('favorites', data.racks[0]);
+			if (!--count) {
+                list.listview();
+			}
+		});
+	});
+}
+
+function writeRack(page, rack,showAvailability) {
+    showAvailability = (typeof showAvailability == "undefined" ? true : false);
+    $('#' + page + ' ul').append( '<li data-id="' + rack.id + '" data-lat="' + rack.geo.coordinates[1] + '" data-lng="' + rack.geo.coordinates[0] + '"' + (typeof rack.distance != "undefined" ? (' data-distance="' + rack.distance + '"'):  '') + '>' +
+            '<a href="#favorites">' + rack.name + 
+            (typeof rack.distance != "undefined" ? (' (' + parseInt( rack.distance * 1000 ) + ' m.)'):  '') +
+            (showAvailability ? '<br>' +
+			(rack.ready_bikes == 0 ? 'Ingen' : rack.ready_bikes) + ' ledige sykler. ' +
+			(rack.empty_locks == 0 ? 'Ingen' : rack.empty_locks) + ' ledige låser.'
+			:'') +
+			'</a></li>');
+}
+
+function addRack() {
+    console.log('add rack');
+    if ( $(this).length != 1 || !parseInt( $(this).attr('data-id') ) ) {
+        return;
+    }
+    console.log('adding...');
+	var racks = localStorage.getItem("racks") ? localStorage.getItem("racks").split(';') : [];
+	racks.push( $(this).attr('data-id') );
+	localStorage.setItem( "racks", racks.join(';')  );
+}
+
+function removeRack(e) {
+	var racks = localStorage.getItem('racks').split(';'), updated_racks = [];
+	var id = $(this).closest('li').attr('data-id');
+	racks.forEach(function(rack, i) {
+	    if (rack && rack != id ) {
+    	    updated_racks.push( rack );
+	    }
+	});
+	localStorage.setItem( "racks", updated_racks.join(';') );
+	$(this).closest('li').hide()
+	$(this).closest('li').remove();
+}
+
+
+window.applicationCache.addEventListener(
+    'updateready',
+    function(){
+        window.applicationCache.swapCache();
+        window.location.reload(true);
+    },
+    false
+);
+    //window.addEventListener('unload', function() { $('body').hide(); } );
+    //document.addEventListener('touchmove', function(e) { e.preventDefault(); } );
+
+$(document).ready( function() {
+    $('#all li').live('click', addRack );
+    $('#nearby li').live('click', addRack );
+    $('#all').bind('beforepageshow', function() {
+        console.log('show all');
+        listAll();
+    });
+    $('#nearby').bind('beforepageshow', function() {
+        console.log('show nearby');
+        listNearby();
+    });
+    $('#favorites').bind('beforepageshow', function() {
+        console.log('show racks');
+        listRacks();
+    });
+
+    
+	var current_location;
+	if ( !window.navigator.onLine ) {
+    	$('#all div[data-role="content"]').html('Du må enten være online eller gå til et bysykkelstativ for å se om det er ledige sykler.');
+	}
+	else {
+    	if ( localStorage.getItem("racks") || !localStorage.getItem('racks') == null ) {
+    	    $.changePage( $('#all'),$('#favorites') );
+    	} else {
+    	    listAll();
+    	}
+	}
+});
---
+++ javascripts/zepto.js
@@ -0,0 +1,159 @@
+var $ = (function(d) {
+  var slice = [].slice, k,jsonpID = (new Date).getTime(),
+    CN = "className", AEL = "addEventListener", PN = "parentNode", QSA = "querySelectorAll",
+    ADJ_OPS = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};
+
+  function $(_, context){
+    if(context !== void 0) return $(context).find(_);
+    function fn(_){ return fn.dom.forEach(_), fn }
+    fn.dom = ((typeof _ == 'function' && 'dom' in _) ? 
+      _.dom : (_ instanceof Array ? _ : 
+        (_ instanceof Element ? [_] : 
+          slice.call(d[QSA](fn.selector = _))))).filter(function(el){
+            return el !== void 0 && el !== null;
+          });
+    for(k in $.fn) fn[k] = $.fn[k];
+    return fn;
+  }
+
+  function classRE(name){ return new RegExp("(^|\\s)"+name+"(\\s|$)") }
+  function elSelect(el, selector){ return slice.call(el[QSA](selector)) }
+
+  $.fn = {
+    get: function(idx){ return idx === void 0 ? this.dom : this.dom[idx] },
+    remove: function(){
+      return this(function(el){ el[PN].removeChild(el) });
+    },
+    each: function(callback){
+      return this(function(e){ callback(e) });
+    },
+    find: function(selector){
+      return $(this.dom.map(function(el){ return elSelect(el, selector) }).reduce(function(a,b){ return a.concat(b) }, []));
+    },
+    closest: function(selector){
+      var el = this.dom[0][PN], nodes = elSelect(d, selector);
+      while(el && nodes.indexOf(el)<0) el = el[PN];
+      return $(el && !(el===d) ? el : []);
+    },
+    show: function(){ return this.css('display:block') },
+    hide: function(){ return this.css('display:none') },
+    prev: function(){ return $(this.dom.map(function(el){ return el.previousElementSibling })) },
+    next: function(){ return $(this.dom.map(function(el){ return el.nextElementSibling })) },
+    html: function(html){
+      return html === void 0 ? (this.dom.length>0 ? this.dom[0].innerHTML : null) : this(function(el){ el.innerHTML = html });
+    },
+    attr: function(name,value){
+      return (typeof name == 'string' && value === void 0) ? (this.dom.length>0 ? this.dom[0].getAttribute(name) || undefined : null) :
+        this(function(el){
+          if (typeof name == 'object') for(k in name) el.setAttribute(k, name[k])
+          else el.setAttribute(name,value);
+        });
+    },
+    offset: function(){
+      var obj = this.dom[0].getBoundingClientRect();
+      return { left: obj.left+d.body.scrollLeft, top: obj.top+d.body.scrollTop, width: obj.width, height: obj.height };
+    },
+    css: function(style){
+      return this(function(el){ el.style.cssText += ';'+style });
+    },
+    index: function(target){
+      return this.dom.indexOf($(target).get(0));
+    },
+    anim: function(transform, opacity, dur){
+      return this.css('-webkit-transition:all '+(dur||0.5)+'s;'+
+        '-webkit-transform:'+transform+';opacity:'+(opacity===0?0:opacity||1));
+    },
+    bind: function(event, callback){
+      return this(function(el){
+        event.split(/\s/).forEach(function(event){ el[AEL](event, callback, false); });
+      });
+    },
+    delegate: function(selector, event, callback){
+      return this(function(el){
+        el[AEL](event, function(event){
+          var target = event.target, nodes = elSelect(el, selector);
+          while(target && nodes.indexOf(target)<0) target = target[PN];
+          if(target && !(target===el) && !(target===d)) callback.call(target, event);
+        }, false);
+      });
+    },
+    addClass: function(name){
+      return this(function(el){
+        !classRE(name).test(el[CN]) && (el[CN] += (el[CN] ? ' ' : '') + name);
+      });
+    },
+    removeClass: function(name){
+      return this(function(el){
+        el[CN] = el[CN].replace(classRE(name), ' ').replace(/^\s+|\s+$/g, '');
+      });
+	}
+  };
+  
+  ['width','height'].forEach(function(m){ $.fn[m] = function(){ return this.offset()[m] }});
+
+  for(k in ADJ_OPS)
+    $.fn[k] = (function(op){
+      return function(html){ return this(function(el){
+        el['insertAdjacent' + (html instanceof Element ? 'Element' : 'HTML')](op,html)
+      })};
+    })(ADJ_OPS[k]);
+
+  ['swipe', 'doubleTap', 'tap'].forEach(function(m){
+    $.fn[m] = function(callback){ return this.bind(m, callback) }
+  });
+
+  function dispatch(event, target) {
+    var e = d.createEvent('Events');
+    e.initEvent(event, true, false);
+    target.dispatchEvent(e);
+  }
+
+  d.ontouchstart = function(e) {
+    var now = Date.now(), t = e.touches[0].target, delta = now - (t.last || now);
+    t.x1 = e.touches[0].pageX;
+    if (delta > 0 && delta <= 800) {
+      dispatch('doubleTap', t);
+      t.last = 0;
+    } else t.last = now;
+  }
+
+  d.ontouchmove = function(e) {
+    e.touches[0].target.x2 = e.touches[0].pageX;
+  }
+
+  d.ontouchend = function(e) {
+    var t = e.target;
+    if (t.x2 > 0) {
+      t.x1 - t.x2 > 60 && dispatch('swipe', t);
+      t.x1 - t.x2 < -60 && dispatch('swipe', t);
+      t.x1 = t.x2 = t.last = 0;
+    } else if (t.last != 0) {
+      dispatch('tap', t);
+    }
+  }
+
+  function ajax(method, url, success, params){
+      var re = /callback=\?/;
+      if (re.test(url)) { // JSONP
+        var jsonpString = 'jsonp' + ++jsonpID;
+        window[jsonpString] = function(j) { success(j) }
+        var script = d.createElement('script');
+        $(script).attr({ src: url.replace(re, 'callback=' + jsonpString), type: 'text/javascript' });
+        $('head').append(script);
+      } else {
+        var r = new XMLHttpRequest();
+        r.onreadystatechange = function(){
+          if(r.readyState==4 && (r.status==200 || r.status==0))
+            success(r.responseText);
+        };
+        r.open(method,url,true);
+        r.setRequestHeader('X-Requested-With','XMLHttpRequest');
+        r.send(params || null);
+      }
+    }
+    $.get = function(url, success, params){ ajax('GET', url + (params || ''), success); };
+    $.post = function(url, success, params){ ajax('POST', url, success, params); };
+    $.getJSON = function(url, success, params){
+      $.get(url, function(json){ success(typeof json == 'string' ? JSON.parse(json) : json) }, params);
+    };  return $;
+})(document);
